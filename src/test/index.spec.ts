71e954186c6b0cb96ead32e0adf1ef35571cd1a3
Martin Rubio <martinrubio23@gmail.com>
2017-06-25 19:32:41 +0100

    [S] Add tests for index.js

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,7 +1,44 @@
 import * as test from 'tape';
 import {Test} from 'tape';
+import * as proxy from 'proxyquire';
+import { stub, spy } from 'sinon';
+
+function getAmqplibMock() {
+  return {
+    connect: stub().returns(Promise.resolve({
+      createChannel: stub().returns(spy())
+    }))
+  };
+}
+
+const setupAct = (spy: any) => ({default: stub().returns(Promise.resolve(spy))});
+const setupAdd = (spy: any) => ({default: stub().returns(Promise.resolve(spy))});
 
 test('This passes', (t: Test) => {
   t.assert(true, 'Simple assertion');
   t.end();
 });
+
+test('Tests setupp funcion' , (t: Test) => {
+
+  const amqplib = getAmqplibMock();
+  const add = spy();
+  const addModule = setupAdd(add);
+  const act = spy();
+  const actModule = setupAct(act);
+  const indexModule = proxy('../main/index', {
+    'amqplib': amqplib,
+    './add': addModule,
+    './act': actModule
+  });
+
+
+  indexModule.default({url: 'urls', exchange: 'ex', queue: 'q'})
+    .then((result: any) => {
+      t.ok(addModule.default.calledOnce, 'Sets up add function');
+      t.equal(result.add, add, 'Returns an initialized add function');
+      t.equal(result.act, act, 'Returns an initialized act function');
+      t.end();
+    });
+});
+
