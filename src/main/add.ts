3d2561e5c16287e643910cf1fc8b55709c54b9a6
Ruben Paz <me@ruben.io>
2017-06-27 12:26:21 +0100

    [S] Basic tests on add

--- src/main/add.ts
+++ src/main/add.ts
@@ -21,6 +21,7 @@ export default async function setupAdd(ch: Channel, options: LibOptions) {
     const queue = _opts.queue_namespace || v4();
     //TODO Match for invalid patterns.
     const queueName = `${pattern}-${queue}`;
+    const errorName = `${pattern}-${queue}-error`;
     await all([
       ch.assertQueue(queueName),
       ch.prefetch(1),
@@ -38,12 +39,22 @@ export default async function setupAdd(ch: Channel, options: LibOptions) {
                 );
               }
             })
+            .catch((err?: Error) => {
+              return all([
+                ch.sendToQueue(
+                  errorName,
+                  msg.content,
+                  {correlationId: msg.properties.correlationId}
+                ),
+                ch.sendToQueue(
+                  msg.properties.replyTo,
+                  Buffer.from(JSON.stringify({error: (err && err.message) || 'Unexpected error'})),
+                  {correlationId: msg.properties.correlationId}
+                )
+              ]);
+            })
             .then(() => {
               ch.ack(msg);
-            })
-            .catch(err => {
-              // TODO : Move message to error queue
-              console.error(err);
             });
         },
         {noAck: false}
--- src/main/types.ts
+++ src/main/types.ts
@@ -4,3 +4,16 @@ export interface LibOptions {
   url: string;
   exchange: string;
 }
+
+export class RCPError extends Error {
+  constructor(msg: string) {
+    super(msg);
+  }
+
+  write() {
+    return `{
+      "_type": "RCPError",
+      "error": ${this.message}
+    }`;
+  }
+}
