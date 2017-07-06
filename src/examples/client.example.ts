17adc33f3fa6c82eb1a89fcd247f8bddaf63d981
Ruben Paz <me@ruben.io>
2017-07-06 01:03:36 +0100

    [S] Examples

---
+++ src/examples/client.example.ts
@@ -0,0 +1,21 @@
+
+import irisSetup from '..';
+
+const config = {
+  url: 'amqp://repositive:repositive@localhost:5672',
+  exchange: 'test'
+};
+
+irisSetup(config)
+  .then(({ act }) => {
+    const sendToTest = act({pattern: 'test'});
+
+    async function work() {
+      const result1 = await sendToTest({secret: 'chourizo'});
+      const result2 = await sendToTest({secret: 'pemento'});
+
+      return {result1, result2};
+    }
+
+    work().then(console.log);
+  });
---
+++ src/examples/serving.example.ts
@@ -0,0 +1,21 @@
+
+import irisSetup from '..';
+
+const config = {
+  url: 'amqp://repositive:repositive@localhost:5672',
+  exchange: 'test'
+};
+
+irisSetup<any>(config)
+  .then(({ add }) => {
+
+    return add({pattern: 'test', async implementation(msg) {
+      return {
+        handled: msg.secret || true
+      };
+    }});
+  })
+  .then(() => {
+    console.log(`Connection stablished using the ${config.exchange} exchange`);
+  })
+  .catch(console.error);
