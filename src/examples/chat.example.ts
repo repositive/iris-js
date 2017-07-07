cfaefa046e1d8b51106b20764b4593d56c83265e
Martin Rubio <martinrubio23@gmail.com>
2017-07-07 15:59:44 +0100

    Add comands

--- src/examples/chat.example.ts
+++ src/examples/chat.example.ts
@@ -32,8 +32,55 @@ interface Msg {
 }
 
 async function chatListener(msg: any): Promise<any> {
-  console.log(`${msg.author}>> ${msg.comment}`);
+  console.log(`${msg.author}: ${msg.comment}`);
   return {ack: true};
+}
+
+function prepareNameListener({username}: {username: string}) {
+  return async function nameListener(msg: any): Promise<any> {
+    return {
+      name: username
+    };
+  };
+}
+
+type ChatParams = {
+  act: (params: {pattern: string}) => (payload: any) => Promise<any>,
+  username: string,
+  target: string,
+  _question?: typeof question,
+  _cmds?: typeof cmds
+};
+
+const cmds: {[k: string]: (params: {oldParams: ChatParams, _question?: typeof question}) => Promise<ChatParams>} = {
+  'cntg': async ({oldParams, _question = question}) => {
+    const target = await _question({query: 'New target: '});
+    return {...oldParams, target};
+  }
+};
+
+async function chat({
+  act,
+  username,
+  target,
+  _question = question,
+  _cmds = cmds
+}: ChatParams): Promise<void> {
+
+  const talk = act({pattern: `chat.${target}`});
+
+  while (true) {
+    const msg = await _question({query: ''});
+    if(/:[a-z]/.test(msg)) {
+      const cmd = msg.substring(1);
+      return chat(await _cmds[cmd]({oldParams: {act, username, target, _question}}));
+    } else {
+      talk({
+        author: username,
+        comment: msg
+     });
+    }
+  }
 }
 
 async function init(
@@ -45,20 +92,16 @@ async function init(
   _irisSetup?: typeof irisSetup
 }): Promise<void> {
   const {act, add} = await _irisSetup(config);
+
   const username = await _question({query: 'Your username: '});
 
   add({pattern: `chat.${username}`, implementation: chatListener});
 
-  const talkTo = await _question({ query: 'Insert user: '});
+  add({pattern: `chat.name.${username}`, implementation: prepareNameListener({username})});
 
-  const talkToUser = act({pattern: `chat.${talkTo}`});
-  while (true) {
-    const msg = await _question({query: ''});
-    talkToUser({
-      author: username,
-      comment: msg
-    });
-  }
+  const target = await _question({ query: 'Insert user: '});
+
+  return chat({act, username, target});
 }
 
 init({}).catch(console.error);
