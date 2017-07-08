e27cffd03031a7e7b0737a82d1d703345109efb5
Ruben Paz <me@ruben.io>
2017-07-08 20:07:30 +0100

    Fix tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -10,12 +10,17 @@ test('Tests setup funcion' , (t: Test) => {
 
   const add = spy();
   const act = spy();
-  const _connect = stub();
+  const _connect = stub().returns({
+    createChannel: stub(),
+    on: spy()
+  });
 
   iris({url: 'urls', exchange: 'ex', _setupAct: setupAct(act), _setupAdd: setupAdd(add), _connect})
     .then((result: any) => {
-      t.equal(result.add, add, 'Returns an initialized add function');
-      t.equal(result.act, act, 'Returns an initialized act function');
+      result.add({pattern: ''});
+      t.ok(add.calledOnce, 'Returns an initialized add function');
+      result.act();
+      t.ok(act.calledOnce, 'Returns an initialized act function');
       t.end();
     });
 });
