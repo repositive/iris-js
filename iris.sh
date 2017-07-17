77995ce4d4eeacc45585e18d3573054347ef2495
Ruben Paz <me@ruben.io>
2017-07-17 15:01:06 +0100

    [B] Fix spacing

--- iris.sh
+++ iris.sh
@@ -7,4 +7,6 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-node $DIR/dist/cli.js $@
+
+C=''
+node $DIR/dist/cli.js "$@"
