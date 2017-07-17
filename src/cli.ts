9844f6631139b9b9673523c9e3f2675b7702d306
Martín Rubio <martinrubio23@gmail.com>
2017-07-17 17:16:29 +0100

    [F] Iris test utility (#23)
    * Add test belt utility
    
    * Default options
    
    * Fix serialization
    
    * Do not print unnecesary stuff
    
    * Do not include cli on tests
    
    * Ignore CLI file in coverage
    
    * [B] Fix spacing
    
    * Add iri.sh to package. Remove comments

---
+++ src/cli.ts
@@ -0,0 +1,51 @@
+import * as yargs from 'yargs';
+import irisSetup from '.';
+
+
+async function handler() {
+  const args = yargs
+    .strict()
+    .usage('Usage: $0  <pattern> [payload]')
+    .nargs('pattern', 1)
+    .option('payload', {
+      describe: 'Object to send to the patternt your requesting',
+        type: 'string',
+        alias: 'p',
+        default: '{}'
+    })
+    .option('uri', {
+      describe: 'Location of the exchange',
+        type: 'string',
+        alias: 'u'
+    })
+    .option('exchange', {
+      describe: 'The exchange used',
+        type: 'string',
+        alias: 'e'
+    })
+    .argv;
+
+  const {_, p, e, u} = args;
+  const [pattern] = _;
+  const payload = p;
+  const uri = u || 'amqp://guest:guest@localhost';
+
+  if (!pattern) {
+    console.log('You must specify a pattern');
+  } else {
+    const _serialization = {
+      parse: (b: Buffer) => b.toString(),
+      serialize: (str: string) => Buffer.from(str)
+    };
+    const iris = await irisSetup({uri, exchange: e || 'iris', _serialization});
+    const res =  await iris.request( {pattern, payload});
+
+    console.log(res);
+    process.exit(0);
+  }
+}
+
+handler().catch((err) => {
+  console.error(err.message);
+  process.exit(1);
+});
