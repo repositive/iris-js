912b1b1f1da59d1608a80ceecf539833c2b22bfd
Ruben Paz <me@ruben.io>
2017-08-30 23:58:42 +0100

    [F] Collect functionality

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import _IrisAMQP from './amqp';
 import {LibOpts as IrisAMQPLibOpts} from './amqp';
 import {parse, serialize} from './serialization';
-import {curry, pipeP, lensProp, over} from 'ramda';
-
+import {is, identity, ifElse, map, curry, pipeP, lensProp, over} from 'ramda';
+import {RPCError} from './errors';
 export const IrisAMQP = _IrisAMQP;
 
 type F1<T, R> = (t: T) => R;
@@ -17,6 +17,8 @@ const parsePayload = toPromise(over(lensPayload, parse));
 
 const serializeP = toPromise(serialize);
 
+const parseArray = toPromise(map(ifElse(is(Buffer), parse, identity))) as any;
+
 const transformHandler = toPromise(over(lensHandler, (handler) => pipeP(parsePayload, handler, serializeP)));
 
 export default async function(opts: (IrisAMQPLibOpts & {
@@ -28,6 +30,7 @@ export default async function(opts: (IrisAMQPLibOpts & {
 
 
   type RequestInput<T> = {pattern: string, payload?: T, timeout?: number};
+  type CollectInput<T> = RequestInput<T>;
   type EmitInput<T> = {pattern: string, payload?: T};
   type RegisterInput<P, R> = {
     pattern: string,
@@ -50,9 +53,16 @@ export default async function(opts: (IrisAMQPLibOpts & {
     backend.emit
   );
 
+  const collect: <T, R> (o: CollectInput<T>) => Promise<(R | RPCError)[]> = pipeP(
+    serializePayload,
+    backend.collect,
+    parseArray
+  );
+
   return {
     request,
     register,
-    emit
+    emit,
+    collect
   };
 }
