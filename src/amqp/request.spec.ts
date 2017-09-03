c34118e1fd5f15d2a59008fc8ed8d1e971de5581
Ruben Paz <me@ruben.io>
2017-09-03 21:03:07 +0100

    [S] Add tests for collect

--- src/amqp/request.spec.ts
+++ src/amqp/request.spec.ts
@@ -101,3 +101,83 @@ test('Test request', (t: Test) => {
     .then(() => t.end())
     .catch(console.error);
 });
+
+test('Test collect', (t: Test) => {
+
+  const ch = mockChannel();
+  const exchange = '';
+
+  async function test() {
+    const pSetupRequest = setupRequest({ch, exchange});
+
+    const requests = await pSetupRequest;
+
+    const pattern = '';
+    const payload = Buffer.from('');
+
+    const pResult1 = requests.collect({pattern, payload});
+
+    t.ok(pResult1 instanceof Promise, 'Collect returns a promise');
+
+    await pResult1
+      .then((res) => {
+        t.deepEqual(res, [], 'If there is no response collect returns and empty array');
+      })
+      .catch(err => {
+        t.notOk(true, 'Collect nevel fails');
+      });
+
+    ch.publish.reset();
+    const pResult2 = requests.collect({pattern, payload});
+
+    await wait(0);
+    const pCall = ch.publish.getCall(0);
+    t.deepEquals(ch.publish.calledOnce && pCall.args[2], payload, 'Publishes the payload');
+    const cCall = ch.consume.getCall(0);
+
+    const r = Math.random();
+    const content = Buffer.from(JSON.stringify({r}));
+    const properties = pCall.args[3];
+    properties.headers = {code: 0};
+    ch.consume.getCall(0).args[1]({content, properties});
+
+    await pResult2
+      .then((result) => {
+        t.deepEquals([Buffer.from(JSON.stringify({r}))], result, 'On success get the expected result');
+      })
+      .catch((err) => {
+        t.notOk(true, 'On success it should not reject');
+      });
+
+    ch.publish.reset();
+
+    const pResult3 = requests.collect({pattern, payload});
+
+    await wait(0);
+
+    const errContent = Buffer.from(JSON.stringify({r}));
+    const pCall3 = ch.publish.getCall(0);
+    const prop3 = pCall3.args[3];
+    prop3.headers = {code: 1};
+    ch.consume.getCall(0).args[1]({content: errContent, properties: prop3});
+
+    await pResult3
+      .then((result) => {
+        t.ok(result, 'If it contacts the server it returns the array always');
+        if (Array.isArray(result)) {
+          const [err] = result;
+          t.ok(err instanceof RPCError, 'The error is an instance of RPCError');
+          if (err instanceof RPCError) {
+            t.deepEquals(err.message, JSON.stringify({r}), 'On error get the expected error message');
+          }
+        }
+      })
+      .catch((err) => {
+        t.notOk(true, 'On error collect does not fail, just wraps the error in the array');
+      });
+  }
+
+  test()
+    .then(() => t.end())
+    .catch(console.error);
+});
